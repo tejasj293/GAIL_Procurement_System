4f42d06bd8c5f8b7deceda37a3c5e680b728c282
tejasj293 <tejasj293@gmail.com>
2020-01-13 11:46:40 +0530

    Running on Lite Server

---
+++ bs-config.js
@@ -0,0 +1,32 @@
+var express    = require("express");
+var app        = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/add", function (req, res) {
+    res.render("addTender");
+});
+
+app.get("/view", function (req, res) {
+    // res.render("");
+});
+
+module.exports = {
+    "server": {
+        "baseDir": "src",
+        "routes": {
+          "/node_modules": "node_modules"
+        },
+        middleware: {
+          1: app,
+      },
+    },
+    port:3000,
+};
